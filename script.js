b1fd96d878ee7c4fc4939819a62c9e7131deaf6f
MaedeSiroos <maede.siroos@gmail.com>
2024-11-21 12:58:59 +0330

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const slides = document.querySelectorAll(".slide")
-const sliderModule = function () {
+const showslide = function () {
   let counter = 0
   let lastItem = 0
-  const sliderTimer = 250
+  const timer = 250
   let showSlide = setTimeout(function show() {
     slides[lastItem].classList.remove("active-slide")
     slides[counter].classList.add("active-slide")
     lastItem = counter
     counter < slides.length - 1 ? counter++ : (counter = 0)
-    showSlide = setTimeout(show, sliderTimer * 16)
-  }, sliderTimer)
-}
-sliderModule()
+    showSlide = setTimeout(show, timer * 16) }, timer) }
+showslide()
